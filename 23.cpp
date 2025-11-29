9ec4326dafe5edc48ce1bf7e24353a6e8aecb948
jitesh <iamjiteshsaini2004@gmail.com>
2025-11-29 18:31:41 +0530

    file updated

--- 23.cpp
+++ 23.cpp
@@ -1,32 +1,42 @@
-// 23. Using ( for loop and while loop) iterate and start with 1 to nth number jump by 
+// 23. Using ( for loop and while loop) iterate and start with 1 to nth number jump by
 //  2
 // 4
 // 5
-// 6 
+// 6
 // 8
 // 10
-
 
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
     cout << "Enter n: ";
     cin >> n;
 
-    for (int i = 1; i <= n; i = i + 2) {
-        cout<<"Output: " << i << " ";
+    cout << "Jump by 2: ";
+    for (int i = 1; i <= n; i = i + 2)
+    {
+        cout << i << " ";
     }
+    cout << endl;
 
     /*
-    int j = 1;
-    while (j <= n) {
-        cout << j << " ";
-        j = j + 2; // Yahan jump lag rahi hai
-    }
-    */
+  int j = 1;
+  while (j <= n) {
+      cout << j << " ";
+      j = j + 2;
+  }
+  */
 
-    cout << endl;
+    // for n = 4,5,6,8,10 
+    // i + 4, j + 4
+    // i + 5, j + 5
+    // i + 6, j + 6
+    // i + 8, j + 8
+    // i + 10, j + 10 aa jayega
+   
+
     return 0;
 }
