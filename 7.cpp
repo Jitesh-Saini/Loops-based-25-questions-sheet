8cb5f9ee0d3cf65656b707a5a6a9497aad5ebe0d
jitesh <iamjiteshsaini2004@gmail.com>
2025-11-29 18:38:51 +0530

    code updated

--- 7.cpp
+++ 7.cpp
@@ -1,26 +1,43 @@
-//7. Check if a number is a palindrome ( using while and do-while loop )
-// Example: 121 → palindrome
+// 7. Check if a number is a palindrome ( using while and do-while loop )
+//  Example: 121 → palindrome
 
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n, original, reversed = 0;
     cout << "Enter a number: ";
     cin >> n;
 
     original = n;
 
-    while (n > 0) {
-        int digit = n % 10;         // last digit
+    while (n > 0)
+    {
+        int digit = n % 10; // last digit
         reversed = reversed * 10 + digit;
-        n = n / 10;               // remove last digit
+        n = n / 10; // remove last digit
     }
 
     if (original == reversed)
         cout << "Palindrome number hai!" << endl;
     else
         cout << "Palindrome number nahi hai!" << endl;
+
+    // n = original; // reversed check ke baad value reset
+    // reversed = 0;
+
+    // do
+    // {
+    //     int digit = n % 10;
+    //     reversed = reversed * 10 + digit;
+    //     n = n / 10;
+    // } while (n > 0);
+
+    // if (original == reversed)
+    //     cout << "Do-While se: Palindrome number hai!" << endl;
+    // else
+    //     cout << "Do-While se: Palindrome number nahi hai!" << endl;
 
     return 0;
 }
